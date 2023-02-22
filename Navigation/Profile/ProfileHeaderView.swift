0715c76833ee1fa1a4206e6e39b386cebfa17f3e
«Daniil <danilkulikovskiy@gmail.com>
2023-02-22 18:01:24 +0300

    add button

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -17,7 +17,7 @@ class ProfileHeaderView: UIView {
     }()
 
     
-    private let showStatusButton: UIButton = {
+    private let setStatusButton: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Show status", for: .normal)
@@ -31,7 +31,7 @@ class ProfileHeaderView: UIView {
         return button
     }()
     
-    private let photoProfile: UIImageView = {
+    private let avatarImageView: UIImageView = {
         var photo = UIImageView()
         photo.clipsToBounds = true
         photo.translatesAutoresizingMaskIntoConstraints = false
@@ -43,7 +43,7 @@ class ProfileHeaderView: UIView {
         return photo
     }()
     
-    var nameLabel: UILabel = {
+    var fullNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Kanye West"
@@ -52,38 +52,52 @@ class ProfileHeaderView: UIView {
         return label
     }()
     
-    var textField: UITextField = {
+    var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel.text = "Waiting for something"
+        statusLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        statusLabel.textColor = .gray
+        return statusLabel
+    }()
+    
+    var statusTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.text = "Waiting for something"
-        textField.font = .systemFont(ofSize: 14, weight: .regular)
+        textField.font = .systemFont(ofSize: 15, weight: .regular)
         textField.textColor = .black
+        textField.layer.cornerRadius = 16
+        textField.layer.backgroundColor = UIColor.white.cgColor
+        textField.layer.cornerRadius = 12
+        textField.layer.borderColor = UIColor.black.cgColor
         return textField
+    }()
+
+    private let changeTitleButton: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("changeTitle", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.backgroundColor = #colorLiteral(red: 0, green: 0.4780646563, blue: 0.9985368848, alpha: 1)
+        return button
     }()
     
     func button() -> UIButton {
-        showStatusButton
+        setStatusButton
     }
     
-    init() {
-        super.init(frame: .zero)
-        customizeViews()
-    }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
-}
-    
-extension ProfileHeaderView {
     func customizeViews() {
         backgroundColor = .white
         addSubview(profileView)
-        profileView.addSubview(showStatusButton)
-        profileView.addSubview(photoProfile)
-        profileView.addSubview(nameLabel)
-        profileView.addSubview(textField)
+        profileView.addSubview(setStatusButton)
+        profileView.addSubview(avatarImageView)
+        profileView.addSubview(fullNameLabel)
+        profileView.addSubview(statusTextField)
+        profileView.addSubview(statusLabel)
+        profileView.addSubview(changeTitleButton)
         
         NSLayoutConstraint.activate([
             profileView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
@@ -91,27 +105,51 @@ extension ProfileHeaderView {
             profileView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             profileView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             
-            photoProfile.topAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.topAnchor, constant: 16),
-            photoProfile.leadingAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            photoProfile.widthAnchor.constraint(equalToConstant: 100),
-            photoProfile.heightAnchor.constraint(equalToConstant: 100),
+            avatarImageView.topAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 100),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 100),
             
-            showStatusButton.topAnchor.constraint(equalTo: photoProfile.bottomAnchor, constant: 16),
-            showStatusButton.leadingAnchor.constraint(equalTo: profileView.leadingAnchor, constant: 16),
-            showStatusButton.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: -16),
-            showStatusButton.widthAnchor.constraint(equalToConstant: 360),
-            showStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 36),
+            setStatusButton.leadingAnchor.constraint(equalTo: profileView.leadingAnchor, constant: 16),
+            setStatusButton.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: -16),
+            setStatusButton.widthAnchor.constraint(equalToConstant: 360),
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
         
-            nameLabel.topAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.topAnchor, constant: 27),
-            nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            nameLabel.leadingAnchor.constraint(equalTo: photoProfile.trailingAnchor, constant: 26),
+            fullNameLabel.topAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.topAnchor, constant: 27),
+            fullNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 26),
             
-            textField.bottomAnchor.constraint(equalTo: showStatusButton.topAnchor, constant: -34),
-            textField.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            textField.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: -16)
+            statusLabel.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -54),
+            statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusLabel.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: -16),
+            
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 6),
+            statusTextField.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusTextField.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: -16),
+            statusTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            changeTitleButton.leadingAnchor.constraint(equalTo: profileView.leadingAnchor, constant: 0),
+            changeTitleButton.trailingAnchor.constraint(equalTo: profileView.trailingAnchor, constant: 0),
+            changeTitleButton.bottomAnchor.constraint(equalTo: profileView.safeAreaLayoutGuide.bottomAnchor),
+            changeTitleButton.heightAnchor.constraint(equalToConstant: 50),
+            
             
            ])
          
     }
-  
+
+
+    init() {
+        super.init(frame: .zero)
+        customizeViews()
+        layoutSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
+
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,23 +11,49 @@ final class ProfileViewController: UIViewController {
     
     private let profileView = ProfileHeaderView()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Profile"
-        profileView.button().addTarget(self, action: #selector(actionButton), for: .touchUpInside)
-        
+        buttonTarget()
+       
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         profileView.frame = view.frame
         view.addSubview(profileView)
-     
+        
+        
+        
     }
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//    }
+    
+    
+   
+   
+    
+    func buttonTarget() {
+        profileView.button().addTarget(self, action: #selector(actionButton), for: .touchUpInside)
+    }
+    
     @objc func actionButton() {
         print("Статус")
     }
     
-}
+//    func customizeViews() {
+//
+//            
+//            NSLayoutConstraint.activate([
+//                profileView.view().topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//                profileView.view().leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+//                profileView.view().trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+//                profileView.view().heightAnchor.constraint(equalToConstant: 220),
+//                
+//            ])
+//        }
+    }
+    
