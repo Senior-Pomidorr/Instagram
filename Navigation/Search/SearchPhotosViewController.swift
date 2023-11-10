0eed7b6af79a4dc8ff0bf6e99e94e1e65b1a96a5
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-11-11 01:25:30 +0300

    fix tabBar, add search collectionView

---
+++ Navigation/Search/SearchPhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchPhotosVC.swift
+//  Navigation
+//
+//  Created by Daniil Kulikovskiy on 10.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class SearchPhotosVC: UICollectionViewController {
+    
+    ove
+}
